0cfa639223f374412d3dbd1e21c700e72323132d
David Griffiths <dogriffiths@gmail.com>
2021-06-09 11:55:59 +0100

    feat: Read actual feed URL

--- seedpod/src/Episodes.js
+++ seedpod/src/Episodes.js
@@ -1,4 +1,37 @@
+import Parser from 'rss-parser'
+import {useEffect, useState} from "react";
+
+const parser = new Parser()
+
+const useFeed = (url) => {
+    const [feed, setFeed] = useState()
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        let canceled = false;
+        setLoading(true);
+        (async () => {
+
+            const f = await parser.parseURL(url);
+
+            f.items.forEach(item => {
+                console.log(item.title + ':' + item.link)
+            });
+
+            if (!canceled) {
+                setFeed(f)
+            }
+            setLoading(false)
+        })();
+        return () => canceled = true;
+    }, [])
+
+    return {data: feed, loading}
+}
+
 const Episodes = () => {
+    const {data: feed} = useFeed('http://www.example.com/podcast.rss')
+
     return <table>
         <thead>
         <tr>
@@ -7,14 +40,14 @@ const Episodes = () => {
         </tr>
         </thead>
         <tbody>
-        <tr>
-            <td>Episode 1</td>
-            <td>All about fish</td>
-        </tr>
-        <tr>
-            <td>Episode 2</td>
-            <td>All about dogs</td>
-        </tr>
+        {
+            feed && feed.items && feed.items.map(i => (
+            <tr>
+            <td>{i.title}</td>
+            <td>{i.content}</td>
+            </tr>
+            ))
+        }
         </tbody>
     </table>
 }
