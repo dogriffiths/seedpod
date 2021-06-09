c5877dad1fcb680f75f29c852e6b78854977356a
David Griffiths <dogriffiths@gmail.com>
2021-06-09 17:52:40 +0100

    feat: Show HTML descriptions

--- seedpod/src/Episode.js
+++ seedpod/src/Episode.js
@@ -1,9 +1,10 @@
+import parse from 'html-react-parser'
 import EpisodeMedia from "./EpisodeMedia";
 
 const Episode = ({episode, feed}) => {
     return <div className='Episode'>
         <div className='title'>{episode.title}</div>
-        <div className='description'>{episode.content}</div>
+        <div className='description'>{parse(episode.content)}</div>
         <div className='duration'>{episode.itunes.duration}</div>
         <EpisodeMedia episode={episode}/>
         <img className='image' src={episode.itunes.image || feed.itunes.image} width={300} height={300} alt='Episode illustration'/>
