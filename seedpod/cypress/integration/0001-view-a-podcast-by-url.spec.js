acf0fd9062be8d449e0276c7673c73cee96bdb7f
David Griffiths <dogriffiths@gmail.com>
2021-06-09 10:50:27 +0100

    chore: Add Cypress and outline first test

---
+++ seedpod/cypress/integration/0001-view-a-podcast-by-url.spec.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+context('Actions', () => {
+    it('should show me the episodes on a podcast', () => {
+        // Given I know what is on a podcast
+        // When I choose to view the episodes for the podcast's URL
+        // Then I should see the correct episodes
+    })
+})
