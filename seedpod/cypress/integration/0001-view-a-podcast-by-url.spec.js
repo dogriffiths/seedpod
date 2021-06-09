c128694151942c44ec5a8cfbb79d51c8ab143a76
David Griffiths <dogriffiths@gmail.com>
2021-06-09 10:58:44 +0100

    test: Start to write first test

--- seedpod/cypress/integration/0001-view-a-podcast-by-url.spec.js
+++ seedpod/cypress/integration/0001-view-a-podcast-by-url.spec.js
@@ -3,7 +3,15 @@
 context('Actions', () => {
     it('should show me the episodes on a podcast', () => {
         // Given I know what is on a podcast
+        cy.intercept('http://www.example.com/podcast.rss', {
+            statusCode: 200,
+            body: 'Hello',
+        })
         // When I choose to view the episodes for the podcast's URL
+        cy.visit('/')
+        cy.get('.urlFeed').type('http://www.example.com/podcast.rss')
+        cy.contains('View URL').click()
         // Then I should see the correct episodes
+
     })
 })
